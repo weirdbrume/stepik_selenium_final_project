fcfe8db1c30ce0b4b91294d287b78d85eab88767
weirdbrume <weirdbrume@gmail.com>
2019-11-18 19:13:23 +0500

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,10 @@
 # task - https://stepik.org/lesson/238819/step/2?unit=211271
+
+from base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
