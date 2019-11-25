2b95a7e0731742f4142fbea00635a13ae1f15f68
weirdbrume <weirdbrume@gmail.com>
2019-11-25 20:56:10 +0500

    add locators to product page locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CLASS_NAME, 'login_form')
     REGISTER_FORM = (By.CLASS_NAME, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, 'btn-add-to-basket')
+    ALERTS_LOCATOR = (By.CLASS_NAME, 'alert-safe')
+    PRODUCT_NAME = (By.CLASS_NAME, 'product_main')
+    PRODUCT_LOCATOR = (By.CLASS_NAME, 'product_main')
