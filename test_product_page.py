efe90fcc9b1bb16742b56760deba41e654a00fc6
weirdbrume <weirdbrume@gmail.com>
2019-11-25 20:58:48 +0500

    update test in test_product_page.py

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.add_product_to_basket()
+
+    product_page.solve_quiz_and_get_code()
+
+    product_page.should_message_product_in_basket_equal_to_the_product_name()
+    product_page.should_message_price_of_product_in_basket_equal_to_product_price()
